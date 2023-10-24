ae475832e1baeabe47cc1d4ed21073e7e73247f1
uerbzr <nigel.sibbert@gmail.com>
2023-10-24 10:46:42 +0100

    added auth controller

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # csharp-api-auth
+
+## Before running
+
+- You need to create a couple of files:
+```
+touch appsettings.json
+touch appsettings.Development.json
+```
+- You then need to add the following contents to both files:  
+```
+{
+  "Logging": {
+    "LogLevel": {
+      "Default": "Information",
+      "Microsoft.AspNetCore": "Warning"
+    }
+  },
+  "AllowedHosts": "*"
+}
+
+```
+## Packages
+
+install-package bcrypt.net-next 
+
---
+++ exercise.wwwapi/EndPoints/AuthApi.cs
@@ -0,0 +1,63 @@
+﻿using exercise.wwwapi.Models;
+using exercise.wwwapi.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+
+namespace exercise.wwwapi.EndPoints
+{
+    public static class AuthApi
+    {
+        public static void ConfigureAuthApi(this WebApplication app)
+        {
+            app.MapPost("register", Register);
+            app.MapPost("login", Login);
+            app.MapGet("users", GetUsers);
+
+        }
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        private static async Task<IResult> GetUsers(IDatabaseRepository<User> service)
+        {
+            return Results.Ok(service.GetAll());
+        }
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        private static async Task<IResult> Register(UserRequestDto request, IDatabaseRepository<User> service)
+        {
+
+            //user exists
+            if (service.GetAll().Where(u => u.Username == request.Username).Any()) return Results.Conflict(new Payload<UserRequestDto>() { status = "Username already exists!", data = request });
+
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+
+            var user = new User();
+
+            user.Username = request.Username;
+            user.PasswordHash = passwordHash;
+
+            service.Insert(user);
+            service.Save();
+
+            return Results.Ok(new Payload<UserResponseDto>() { data = new UserResponseDto() { Username = request.Username, PasswordHash = passwordHash } });
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        private static async Task<IResult> Login(UserRequestDto request, IDatabaseRepository<User> service)
+        {
+            //user doesn't exist
+            if (!service.GetAll().Where(u => u.Username == request.Username).Any()) return Results.BadRequest(new Payload<UserRequestDto>() { status = "User does not exist", data = request });
+
+            User user = service.GetAll().FirstOrDefault(u => u.Username == request.Username);
+           
+
+            if (!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+            {
+                return Results.BadRequest(new Payload<UserRequestDto>() { status = "Wrong Password", data = request });
+            }
+
+            return Results.Ok(new Payload<UserResponseDto>() { data = new UserResponseDto() { Username = user.Username, PasswordHash = user.PasswordHash } }) ;
+           
+        }
+    }
+}
+
